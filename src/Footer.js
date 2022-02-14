f5f0381e9a6bf10f35a2ee5204ea0bd679dc9ae7
Olga Narvaez <olguism14@gmail.com>
2022-02-14 18:09:03 -0500

    footer updated

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-        <p> Jokes Challenge - React Course - Beginner's Tutorial for React JavaScript Library coded by: <a href="https://github.com/olga-narvaez">
+        <p> Jokes Challenge - React Course - Beginner's Tutorial for React JavaScript Library coded by: <a href="https://github.com/olga-narvaez/jokes-app">
             Olga Narvaez
         </a> </p>
     </div>
