28403780d7c5d63802a175124d4ab89e7062d024
Olga Narvaez <olguism14@gmail.com>
2022-02-15 07:14:29 -0500

    butttons added

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-        <h1> DEV JOKES </h1>
+      <h1> DEV JOKES </h1>
     </div>
   );
 }
--- src/Joke.js
+++ src/Joke.js
@@ -1,10 +1,21 @@
 import React from "react";
+import like from "./images/like.svg";
+import dislike from "./images/dislike.svg";
 
 export default function Joke(props) {
+  console.log(props.isPun);
   return (
     <div className="jokes">
       {props.setup && <h3> Setup: {props.setup} </h3>}
       <p className="light-text"> Punchline: {props.punchline} </p>
+      <div className="reactions">
+        <button>
+          <img src={like} alt="like button" /> {props.upvotes}
+        </button>
+        <button>
+          <img src={dislike} alt="dislike button" /> {props.downvotes}
+        </button>
+      </div>
     </div>
   );
 }
